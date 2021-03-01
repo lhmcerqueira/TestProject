13f0acca39f6586830c9be09e1ed69de768d417a
lhmcerqueira <luizhenriquemc1@gmail.com>
2021-03-01 19:30:27 -0300

    creating the master branch

---
+++ src/main/java/helloworld/Main.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        String name;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Please enter your name:/n");
+        name = sc.nextLine();
+        System.out.println("Hello "+name+", it's working!");
+    }
+}
